660cc78af377e25d53b5f8fe4b8e1f713d0ed673
Carlos Alencar Bloise (Nokia) <carlos.alencar_bloise@nokia.com>
2023-03-17 15:19:44 -0300

    add pictures

--- js.js
+++ js.js
@@ -1,30 +1,79 @@
 let slideAtual = 0;
-const slides = document.querySelectorAll('.slider-section img');
 
 const slider1 = document.querySelector('.slider1');
 const slider2 = document.querySelector('.slider2');
 const video = document.querySelector('.video');
 
-function mudarSlide(n) {
+var imagens = [
+	"cub0",
+	"cub1",
+	"cub2",
+	"cub3",
+	"cub4",
+	"cub5",
+	"cub6",
+	"cub7",
+	"cub8",
+	"cub9",
+	"cub10",
+	"cub11",
+	"cub12",
+	"cub13",
+	"cub14",
+];
+
+var divSlider1 = document.querySelector(".slider1");
+var divSlider2 = document.querySelector(".slider2");
+var slides1 = []
+var slides2 = []
+
+for (var i = 0; i < imagens.length; i+=2) {
+	slides1.push(imagens[i])
+	var imagem = "<img src='./imgs/" + imagens[i] + ".jpeg' />";
+	divSlider1.innerHTML += imagem;
+}
+for (var i = 1; i < imagens.length; i+=2) {
+	slides2.push(imagens[i])
+	var imagem = "<img src='./imgs/" + imagens[i] + ".jpeg' />";
+	divSlider2.innerHTML += imagem;
+}
+
+const imgs = document.querySelectorAll('.slider-section img');
+
+function mudarSlide1(n) {
 	slideAtual += n;
-	if (slideAtual > slides.length - 1) {
+	if (slideAtual > slides1.length - 1) {
 		slideAtual = 0;
 	}
 	if (slideAtual < 0) {
-		slideAtual = slides.length - 1;
+		slideAtual = slides1.length - 1;
 	}
-	slider1.scrollTo({
-		left: slideAtual * slides[0].offsetWidth,
-		behavior: 'smooth'
-	});
 
-	slider2.scrollTo({
-		left: slideAtual * slides[0].offsetWidth,
+	
+
+	slider1.scrollTo({
+		left: slideAtual * imgs[0].offsetWidth,
 		behavior: 'smooth'
 	});
 }
 
-setInterval(() => mudarSlide(1), 5000);
+function mudarSlide2(n) {
+	slideAtual += n;
+	if (slideAtual > slides2.length - 1) {
+		slideAtual = 0;
+	}
+	if (slideAtual < 0) {
+		slideAtual = slides2.length - 1;
+	}
+	slider2.scrollTo({
+		left: slideAtual * imgs[0].offsetWidth,
+		behavior: 'smooth'
+	});
+}
+
+console.log(slides1[0].offsetWidth)
+setInterval(() => mudarSlide1(1), 5000);
+setInterval(() => mudarSlide2(1), 5000);
 
 const popup = document.querySelector('.popup');
 const bgPopup = document.querySelector('.bgPopup');
@@ -57,3 +106,5 @@ function createSnowflake() {
 }
 
 setInterval(createSnowflake, 200);
+
+
